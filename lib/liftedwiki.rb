6984cbb3be6cbdf359db53fa63a56f2fd65c14ea
Lee Dohm <lee@liftedstudios.com>
2012-12-23 08:12:52 -0800

    Fixed an issue where the application wouldn't launch.

--- lib/liftedwiki.rb
+++ lib/liftedwiki.rb
@@ -2,8 +2,10 @@
 # Copyright (c) 2012 by Lifted Studios.  All Rights Reserved.
 # 
 
-require 'liftedwiki/frontend/wiki_app'
 require 'liftedwiki/pathname'
 require 'liftedwiki/pipeline'
 require 'liftedwiki/reference_filter'
 require 'liftedwiki/version'
+
+# Load this last because it launches the web application and blocks.
+require 'liftedwiki/frontend/wiki_app'
