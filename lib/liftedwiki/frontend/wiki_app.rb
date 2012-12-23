e6b79745d1422e147cb50a5e136d18fe6adfe357
Lee Dohm <lee@liftedstudios.com>
2012-12-23 09:31:46 -0800

    Enhanced the handler so a 404 is returned if a file doesn't exist.

--- lib/liftedwiki/frontend/wiki_app.rb
+++ lib/liftedwiki/frontend/wiki_app.rb
@@ -7,13 +7,33 @@ require 'sinatra/base'
 module LiftedWiki
   # Controls the wiki application by handling all routing and events.
   class WikiApp < Sinatra::Base
+    def new_pipeline
+      @pipeline = Pipeline.new if @pipeline.nil?
+      @pipeline
+    end
+
     get '/' do
       redirect '/Home'
     end
 
+    get '/favicon.ico' do
+      raise Sinatra::NotFound
+    end
+
     get '*' do
       path = params[:splat].first
-      erb :page, :locals => { :body => "<p>It works!</p>", :title => "#{path}: It Works!" }
+      pass if path =~ /^\/__sinatra__/
+      path = path[1..-1] if path[0] == '/'
+      filename = File.join(Dir.pwd, path + '.md')
+
+      if File.exists?(filename)
+        pipeline = new_pipeline
+        body = pipeline.run(File.read(File.join(Dir.pwd, path + '.md')))
+
+        erb :page, :locals => { :body => body, :title => path }
+      else
+        raise Sinatra::NotFound
+      end
     end
   end
 end
