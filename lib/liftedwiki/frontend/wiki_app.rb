a1f16dd5b09b94a2105429fde1d626f11de3ac21
Lee Dohm <lee@liftedstudios.com>
2012-12-23 11:55:33 -0800

    Added the ability to serve CSS files.

--- lib/liftedwiki/frontend/wiki_app.rb
+++ lib/liftedwiki/frontend/wiki_app.rb
@@ -7,9 +7,50 @@ require 'sinatra/base'
 module LiftedWiki
   # Controls the wiki application by handling all routing and events.
   class WikiApp < Sinatra::Base
+    # Indicates whether the given `path` has a file extension.
+    # 
+    # @param [String] path Path to check.
+    # @return [Boolean] `true` if `path` has an extension; `false` otherwise.
+    def has_extension(path)
+      !File.extname(path).empty?
+    end
+
+    # Gets the pipeline object.
+    # 
+    # Creates a new pipeline object if one does not already exist.
+    # 
+    # @return [Pipeline] Pipeline object to use.
     def pipeline
       @pipeline = Pipeline.new if @pipeline.nil?
       @pipeline
+    end
+
+    # Serves a file.
+    # 
+    # @param [String] path Path to the file to serve.
+    # @return Contents of the file.
+    def serve_file(path)
+      logger.info "Entered serve_file with parameters: #{path}"
+      File.read(File.join(File.dirname(__FILE__), 'css', path))
+    end
+
+    # Serves a wiki page.
+    # 
+    # @param [String] path Path of the wiki page to serve.
+    # @return [String] Content of the page to serve.
+    def serve_page(path)
+      logger.info "Entered serve_page with parameters: #{path}"
+
+      filename = File.join(Dir.pwd, path + '.md')
+      raise Sinatra::NotFound unless File.exists?(filename)
+
+      body = pipeline.run(File.read(filename))
+
+      erb :page, :locals => { :body => body, :title => File.basename(path) }
+    end
+
+    configure :production, :development do
+      enable :logging
     end
 
     get '/' do
@@ -24,12 +65,11 @@ module LiftedWiki
       path = params[:splat].first
       pass if path =~ /^\/__sinatra__/
 
-      filename = File.join(Dir.pwd, path + '.md')
-      raise Sinatra::NotFound unless File.exists?(filename)
-
-      body = pipeline.run(File.read(filename))
-
-      erb :page, :locals => { :body => body, :title => File.basename(path) }
+      if has_extension(path)
+        serve_file(path)
+      else
+        serve_page(path)
+      end
     end
   end
 end
