defb76d777767b316d3acd84a4063114044c96e1
Lee Dohm <lee@liftedstudios.com>
2012-12-24 09:42:11 -0800

    Added a test to ensure that WikiLinkFilter only accepts Strings.

--- lib/liftedwiki/wiki_link_filter.rb
+++ lib/liftedwiki/wiki_link_filter.rb
@@ -5,5 +5,8 @@
 module LiftedWiki
   # An `HTML::Pipeline` filter class that detects wiki-style links and converts them to HTML links.
   class WikiLinkFilter < HTML::Pipeline::Filter
+    def initialize(text)
+      raise ArgumentError, "WikiLinkFilter only supports raw text." unless text.kind_of? String
+    end
   end
 end
--- spec/wiki_link_filter_spec.rb
+++ spec/wiki_link_filter_spec.rb
@@ -8,11 +8,26 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 require 'liftedwiki'
+require 'nokogiri'
 
 describe LiftedWiki::WikiLinkFilter do
+  # Creates a new `WikiLinkFilter` object.
+  # 
+  # @param text Text for the filter to parse.
+  # @return [LiftedWiki::WikiLinkFilter] Newly created filter object.
+  def new_filter(text)
+    LiftedWiki::WikiLinkFilter.new(text)
+  end
+
   it 'can be instantiated' do
-    filter = LiftedWiki::WikiLinkFilter.new('Just some sample text')
+    filter = new_filter('Just some sample text')
 
     filter.wont_be_nil
+  end
+
+  it 'raises an error if given a Nokogiri DocumentFragment' do
+    proc {
+      new_filter(Nokogiri::make('Just some sample text'))
+    }.must_raise ArgumentError
   end
 end
